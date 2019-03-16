cb97c5c00d372465877bd855d47537f0a8a9c2c6
mirza <sugamirza2@gmail.com>
2019-03-17 00:50:57 +0700

    hotfix wilayah regency service

--- app/Http/Controllers/Wilayah/RegencyController.php
+++ app/Http/Controllers/Wilayah/RegencyController.php
@@ -5,21 +5,21 @@ namespace App\Http\Controllers\Wilayah;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Services\Wilayah\RegencyService;
+use App\Services\Wilayah\WilayahRegency;
 use App\Traits\ApiResponser;
 
 class RegencyController extends Controller
 {
     use ApiResponser;
-    protected $regencyService;
+    protected $wilayahRegency;
 
-    public function __construct(RegencyService $regencyService) {
-        $this->regencyService = $regencyService;
+    public function __construct(WilayahRegency $wilayahRegency) {
+        $this->wilayahRegency = $wilayahRegency;
     }
 
     public function all($provinceId, Request $request) {
 
-        $response = $this->regencyService->obtainByParentId($provinceId, $request->all());
+        $response = $this->wilayahRegency->obtainByParentId($provinceId, $request->all());
         $body = json_decode($response);
 
         if ($body->status_code === 200 ) {
