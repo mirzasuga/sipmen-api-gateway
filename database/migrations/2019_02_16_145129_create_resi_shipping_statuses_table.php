6d8dab95f449c756b5eaab331cfa32f06faf4eef
mirza <sugamirza2@gmail.com>
2019-03-17 02:23:53 +0700

    add id in resi_shipping_statuses_table

--- database/migrations/2019_02_16_145129_create_resi_shipping_statuses_table.php
+++ database/migrations/2019_02_16_145129_create_resi_shipping_statuses_table.php
@@ -14,6 +14,7 @@ class CreateResiShippingStatusesTable extends Migration
     public function up()
     {
         Schema::create('resi_shipping_statuses', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('resi_id');
             $table->integer('shipping_status_id');
             $table->string('note');
